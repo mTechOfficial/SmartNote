84a2aa7bd94ec5f6f4e5b729cd422530f0883865
irteza_syed <irteza.syed@macaulay.cuny.edu>
2017-04-11 15:44:27 -0400

    Added random string generator.

---
+++ src/SmartNote/SmartNote/Controllers/CodeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace SmartNote.Controllers
+{
+	class CodeGenerator
+	{
+
+		public CodeGenerator() {}
+		protected const int SIZE = 6;
+		string GenerateCode()
+		{   
+			Random rand = new Random();
+			string Alphabet = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+			char[] chars = new char[SIZE];
+			for (int i = 0; i<SIZE; i++)
+			{
+				chars[i] = Alphabet[rand.Next(Alphabet.Length)];
+			}
+			Console.Write(Alphabet);
+			return Alphabet;
+                }
+	}
+}
