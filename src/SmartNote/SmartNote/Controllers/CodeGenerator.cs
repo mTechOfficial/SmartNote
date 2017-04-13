c9d47b8136dd134667d608677d889b61ed83b7f7
muhand <muhandjumah@gmail.com>
2017-04-13 18:38:13 -0400

    Created project files

---
+++ src/SmartNote/SmartNote/Controllers/CodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿/*
+						 Visibility.cs
+						  Irteza Syed
+						 04/11/2017
+                         Edited By: Zhi Qiu
+                         Changes: Made GenerateCode method public static and removed Console.Write debugging line
+                         04/12/2017
+ Enumeration for visibility type of groups.
+*/
+using System;
+
+namespace SmartNote.Controllers
+{
+    class CodeGenerator
+    {
+        public CodeGenerator() { }
+        protected const int SIZE = 6;
+        public static string GenerateCode()
+        {
+            Random rand = new Random();
+            string Alphabet = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            char[] chars = new char[SIZE];
+            for (int i = 0; i < SIZE; i++)
+            {
+                chars[i] = Alphabet[rand.Next(Alphabet.Length)];
+            }
+            return Alphabet;
+        }
+    }
+}
---
+++ src/SmartNote/SmartNote/Enums/Repeat.cs
@@ -0,0 +1,15 @@
+﻿/*
+						 Repeat.cs
+						  Zhi Qiu
+						 04/10/2017
+ Enumeration for study group repeat option.
+*/
+namespace SmartNote.Enums
+{
+    enum Repeat
+    {
+        Daily,
+        Weekly,
+        Monthly
+    }
+}
